0f9e891ba4d2a75d4bd0e00ba4283a3553be46e2
huangrunhong <huang.run.hong.1986@gmail.com>
2023-10-31 14:01:38 +0100

    title erganzen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 // ===========================
 //      Array-Level-1_1
 // ===========================
+console.log("%c=====Array-Level-1_1=====", "color:yellowgreen");
 const arrayPerson1 = ["Bruce", "Wayne", "Superreich"];
 const arrayPerson2 = ["Farid", "Christian", "Serio"];
 const arrayPerson3 = ["Burger", "Sushi", "Mais-Käsebällchen"];
@@ -11,6 +12,7 @@ console.log(arrayPerson3);
 // ===========================
 //      Array-Level-1_2
 // ===========================
+console.log("%c=====Array-Level-1_2=====", "color:yellowgreen");
 console.log(arrayPerson1[0]);
 console.log(arrayPerson1[1]);
 console.log(arrayPerson1[2]);
@@ -26,6 +28,7 @@ console.log(arrayPerson3[2]);
 // ===========================
 //      Array-Level-1_3
 // ===========================
+console.log("%c=====Array-Level-1_3=====", "color:yellowgreen");
 console.log(arrayPerson1.length);
 console.log(arrayPerson2.length);
 console.log(arrayPerson3.length);
@@ -33,6 +36,7 @@ console.log(arrayPerson3.length);
 // ===========================
 //      Array-Level-1_4
 // ===========================
+console.log("%c=====Array-Level-1_4=====", "color:yellowgreen");
 console.log(arrayPerson1);
 arrayPerson1.push("Batman", "Gotham-City");
 console.log(arrayPerson1);
@@ -48,6 +52,7 @@ console.log(arrayPerson3);
 // ===========================
 //      Array-Level-1_5
 // ===========================
+console.log("%c=====Array-Level-1_5=====", "color:yellowgreen");
 console.log(arrayPerson1);
 let lastArrayPerson1 = arrayPerson1.pop();
 console.log(lastArrayPerson1);
@@ -66,6 +71,7 @@ console.log(arrayPerson3);
 // ===========================
 //      Array-Level-1_6
 // ===========================
+console.log("%c=====Array-Level-1_6=====", "color:yellowgreen");
 console.log(arrayPerson1);
 let firstArrayPerson1 = arrayPerson1.shift();
 console.log(firstArrayPerson1);
@@ -74,6 +80,7 @@ console.log(arrayPerson1);
 // ===========================
 //      Array-Level-1_7
 // ===========================
+console.log("%c=====Array-Level-1_7=====", "color:yellowgreen");
 console.log(arrayPerson2);
 arrayPerson2.unshift("Julia", "Lea");
 console.log(arrayPerson2);
@@ -85,6 +92,7 @@ console.log(arrayPerson3);
 // ===========================
 //      Array-Level-1_9
 // ===========================
+console.log("%c=====Array-Level-1_9=====", "color:yellowgreen");
 const numArray = [23, 54, 75];
 console.log(numArray);
 numArray.push(11, 32, 42, 5, 71);
